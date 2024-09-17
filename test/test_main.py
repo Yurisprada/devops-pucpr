7517f873e8f47c7a7aa6bb3b7d55ae3fad15ba60
Yuri Gabriel Sprada <yuri_sprada@hotmail.com>
2024-09-17 00:28:13 -0300

    update testes.yml

--- test/test_main.py
+++ test/test_main.py
@@ -1,21 +1,30 @@
 import unittest
+from unittest.mock import patch
 from src.Main import (
     qtdJogadores, cadastro_jogadores, tuboDados, rollDice, rollFace,
-    pontuacaoC, pontuacaoP, pontuacaooT
+    pontuacaoC, pontuacaoP, pontuacaoT, menu, menu2
 )
 
 class TestZombieDice(unittest.TestCase):
-    
-    def test_qtdJogadores(self):
-        num_jogadores = qtdJogadores('1')
+
+    @patch('builtins.input', return_value='1')
+    def test_menu(self, mock_input):
+        op = menu()
+        self.assertEqual(op, '1')  # Deve retornar '1'
+
+    @patch('builtins.input', return_value='2')
+    def test_qtdJogadores(self, mock_input):
+        num_jogadores = qtdJogadores()
         self.assertEqual(num_jogadores, 2)  # Deve retornar 2
 
-    def test_qtdJogadores_valido(self):
-        num_jogadores = qtdJogadores('3')
+    @patch('builtins.input', return_value='3')
+    def test_qtdJogadores_valido(self, mock_input):
+        num_jogadores = qtdJogadores()
         self.assertEqual(num_jogadores, 3)  # Deve retornar 3
 
-    def test_cadastro_jogadores(self):
-        jogadores = cadastro_jogadores(['', 'Player1', 'Player2'])
+    @patch('builtins.input', side_effect=['', 'Player1', 'Player2'])
+    def test_cadastro_jogadores(self, mock_input):
+        jogadores = cadastro_jogadores()
         self.assertEqual(jogadores, ['Player1', 'Player2'])  # Deve conter os dois nomes válidos
 
     def test_tuboDados(self):
@@ -34,29 +43,32 @@ class TestZombieDice(unittest.TestCase):
         dado = rollFace()
         self.assertIn(dado, ['C', 'P', 'T'])  # A face deve ser uma das válidas
 
-    def test_pontuacaoC(self):
-        global pontAtualC, cerebro, jogadorAtual
-        pontAtualC = [0]
-        cerebro = [3]
-        jogadorAtual = 0
+    @patch('src.Main.pontAtualC', [0])
+    @patch('src.Main.cerebro', [3])
+    @patch('src.Main.jogadorAtual', 0)
+    def test_pontuacaoC(self, mock_pontAtualC, mock_cerebro, mock_jogadorAtual):
         pontuacaoC()
         self.assertEqual(pontAtualC[0], 3)  # Deve atualizar a pontuação corretamente
 
-    def test_pontuacaoP(self):
-        global pontAtualP, jogadorAtual
-        pontAtualP = [0]
-        jogadorAtual = 0
+    @patch('src.Main.pontAtualP', [0])
+    @patch('src.Main.jogadorAtual', 0)
+    def test_pontuacaoP(self, mock_pontAtualP, mock_jogadorAtual):
         pontuacaoP()
         self.assertEqual(pontAtualP[0], 1)  # Deve incrementar a pontuação
 
-    def test_pontuacaoT(self):
-        global pontAtualT, cerebro, jogadorAtual
-        pontAtualT = [2]
-        cerebro = [5]
-        jogadorAtual = 0
-        pontuacaooT()
+    @patch('src.Main.pontAtualT', [2])
+    @patch('src.Main.cerebro', [5])
+    @patch('src.Main.jogadorAtual', 0)
+    def test_pontuacaoT(self, mock_pontAtualT, mock_cerebro, mock_jogadorAtual):
+        pontuacaoT()
         self.assertEqual(pontAtualT[0], 3)  # Deve incrementar o tiro
         self.assertEqual(cerebro[0], 0)  # Deve zerar os cérebros, pois os tiros chegaram a 3
+
+
+    @patch('builtins.input', return_value='5')
+    def test_menu2(self, mock_input):
+        op = menu2()
+        self.assertEqual(op, '2')  # Deve retornar '5'
 
 if __name__ == '__main__':
     unittest.main()
