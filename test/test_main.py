56a2167d0466ca0a1de10f8abcc26cf414a9aef4
Yuri Gabriel Sprada <yuri_sprada@hotmail.com>
2024-09-17 23:40:16 -0300

    update testes.yml

--- test/test_main.py
+++ test/test_main.py
@@ -1,21 +1,18 @@
 import unittest
 from unittest.mock import patch
-from src import *
+from src.Main import *
 
 class TestZombieDice(unittest.TestCase):
-    
-    # Testando iniciar o jogo com a opção 1 no menu
-    @patch('builtins.input', side_effect=['1'])  # Simulando escolha da opção 1 (Iniciar Jogo)
-    def test_menu_iniciar_jogo(self, mock_input):
-        op1 = menu()
-        self.assertEqual(op1, '1')
 
-    # Testando finalizar o jogo com a opção 0 no menu
-    @patch('builtins.input', side_effect=['0'])  # Simulando escolha da opção 0 (Finalizar Jogo)
-    def test_menu_finalizar_jogo(self, mock_input):
-        op1 = menu()
-        self.assertEqual(op1, '0')
+    @patch('builtins.input', return_value='1')
+    def test_menu(self, mock_input):
+        op = menu()
+        self.assertEqual(op, '1')  # Deve retornar '1'
 
+    @patch('builtins.input', return_value='5')
+    def test_menu2(self, mock_input):
+        op = menu2()
+        self.assertEqual(op, '2')  # Deve retornar '5'
 
 if __name__ == '__main__':
     unittest.main()
