7894679a9e21594b4e72231003e1aff3488b1ba4
Yuri Gabriel Sprada <yuri_sprada@hotmail.com>
2024-09-18 00:27:34 -0300

    update testes.yml

--- test/test_main.py
+++ test/test_main.py
@@ -3,9 +3,7 @@ from unittest.mock import patch
 from src.Main import *
 
 class TestZombieDice(unittest.TestCase):
-    def test_import_jogo():
-        try:
-            from src.com.jogo import Jogo
-        except ImportError as e:
-            assert False, f"Importação falhou: {e}"
-        assert True  # O teste passa se a importação for bem-sucedida
+    @patch('builtins.input', side_effect=['1'])
+    def test_menu_start_game(self, mock_input):
+        result = menu()
+        self.assertEqual(result, '1')  # Ajuste a comparação conforme o esperado em `menu()`
