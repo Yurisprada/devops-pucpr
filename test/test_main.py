e8f5abe1f205bb17d663d3a0dcce6db997961807
Yuri Gabriel Sprada <yuri_sprada@hotmail.com>
2024-09-18 00:25:53 -0300

    update testes.yml

--- test/test_main.py
+++ test/test_main.py
@@ -2,14 +2,10 @@ import unittest
 from unittest.mock import patch
 from src.Main import *
 
-def test_import_jogo():
-    try:
-        from src.com.jogo import Jogo
-    except ImportError as e:
-        assert False, f"Importação falhou: {e}"
-    assert True  # O teste passa se a importação for bem-sucedida
-
-def test_jogo_instanciacao():
-    from src.com.jogo import Jogo
-    jogo = Jogo()
-    assert jogo is not None  # Verifica se a instância foi criada
+class TestZombieDice(unittest.TestCase):
+    def test_import_jogo():
+        try:
+            from src.com.jogo import Jogo
+        except ImportError as e:
+            assert False, f"Importação falhou: {e}"
+        assert True  # O teste passa se a importação for bem-sucedida
