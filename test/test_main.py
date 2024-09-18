70c237222d27b2474bc20afc9c431a67f602fddf
Yuri Gabriel Sprada <yuri_sprada@hotmail.com>
2024-09-17 23:45:28 -0300

    update testes.yml

--- test/test_main.py
+++ test/test_main.py
@@ -2,8 +2,14 @@ import unittest
 from unittest.mock import patch
 from src.Main import *
 
-@patch('builtins.input', side_effect=['Alice', 'Bob'])
-def test_cadastro_jogadores(self, mock_input):
-    jogadores = cadastro_jogadores()
-    self.assertEqual(jogadores, ['Alice', 'Bob'])
-    print(jogadores)
+def test_import_jogo():
+    try:
+        from src.com.jogo import Jogo
+    except ImportError as e:
+        assert False, f"Importação falhou: {e}"
+    assert True  # O teste passa se a importação for bem-sucedida
+
+def test_jogo_instanciacao():
+    from src.com.jogo import Jogo
+    jogo = Jogo()
+    assert jogo is not None  # Verifica se a instância foi criada
