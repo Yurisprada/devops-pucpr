5ff9bbb1779785524557db9ac2ff9c5ae8a064db
Yuri Gabriel Sprada <yuri_sprada@hotmail.com>
2024-09-18 00:31:26 -0300

    update testes.yml

--- test/test_main.py
+++ test/test_main.py
@@ -3,6 +3,13 @@ from unittest.mock import patch
 from src.Main import *
 
 class TestZombieDice(unittest.TestCase):
+
+    def test_menu():
+    # Simula a entrada do usuário como '1'
+        with patch('builtins.input', return_value='1'):
+            op1 = menu()
+        assert op1 == '1'
+
     @patch('builtins.input', side_effect=['1'])
     def test_menu_start_game(self, mock_input):
         result = menu()
