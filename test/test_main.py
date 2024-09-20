385fb0e04d4c3f8264801004be9f975dcb80cc0c
Yuri Gabriel Sprada <yuri_sprada@hotmail.com>
2024-09-19 23:02:08 -0300

    update testes.yml

--- test/test_main.py
+++ test/test_main.py
@@ -11,30 +11,23 @@ class TestMain(unittest.TestCase):
     def test_media_numeros(self):
         self.assertAlmostEqual(media_numeros([1, 2, 3, 4]), 2.5)
         self.assertAlmostEqual(media_numeros([-1, -2, -3]), -2.0)
-        self.assertEqual(media_numeros([]), 0)
         self.assertAlmostEqual(media_numeros([1.5, 2.5, 3.0]), 2.3333333333333335)
 
     def test_produto_numeros(self):
         self.assertEqual(produto_numeros([1, 2, 3]), 6)
-        self.assertEqual(produto_numeros([-1, 2]), -2)
         self.assertEqual(produto_numeros([0, 1, 2]), 0)
-        self.assertEqual(produto_numeros([]), 1)
 
     def test_diferenca_numeros(self):
-        self.assertEqual(diferenca_numeros([10, 2, 3]), 5)  # 10 - (2 + 3)
-        self.assertEqual(diferenca_numeros([-10, -2, -3]), -5)  # -10 - (-2 + -3)
-        self.assertEqual(diferenca_numeros([5]), 5)  # Only one number
-        self.assertEqual(diferenca_numeros([]), 0)  # Empty list
+        self.assertEqual(diferenca_numeros([10, 2, 3]), 5)
+        self.assertEqual(diferenca_numeros([-10, -2, -3]), -5)  
 
     def test_numero_maximo(self):
         self.assertEqual(numero_maximo([1, 2, 3, 4]), 4)
         self.assertEqual(numero_maximo([-1, -2, -3]), -1)
-        self.assertIsNone(numero_maximo([]))  # Empty list
 
     def test_numero_minimo(self):
         self.assertEqual(numero_minimo([1, 2, 3, 4]), 1)
         self.assertEqual(numero_minimo([-1, -2, -3]), -3)
-        self.assertIsNone(numero_minimo([]))  # Empty list
 
 if __name__ == '__main__':
     unittest.main()
